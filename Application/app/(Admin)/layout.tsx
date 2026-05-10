737616e136df4bfdbcecb61b6dc21d97c0a705f0
Devell Robinson <devell.robinson@bordevtech.com>
2026-05-10 04:57:23 -0400

    feat: add layout components for Admin and Pages sections

--- Application/app/(Admin)/layout.tsx
+++ Application/app/(Admin)/layout.tsx
@@ -1,6 +1,6 @@
+import { Provider } from "@/components/ui/provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +27,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <Provider>
+          {children}
+        </Provider>
       </body>
     </html>
   );
