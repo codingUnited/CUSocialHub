509a95aa8733a7cd6f3e1a044af8d6703604f7e2
Devell Robinson <devell.robinson@bordevtech.com>
2026-05-23 16:40:33 -0400

    refactor: simplify GET function signature and improve code readability

--- Application/app/(Redirect Routes)/redirect/[code]/route.ts
+++ Application/app/(Redirect Routes)/redirect/[code]/route.ts
@@ -1,15 +1,14 @@
 import { NextRequest, NextResponse } from "next/server";
 
-interface Params {
-    params: { code: string };
-}
-
-export const GET = async (req: NextRequest, { params }: Params) => {
-    const url = req.nextUrl.searchParams.get("u");
+export async function GET(
+    request: NextRequest,
+    context: { params: { code: string } }
+) {
+    const url = request.nextUrl.searchParams.get("u");
 
     if (!url) {
         return new NextResponse("Invalid short link", { status: 400 });
     }
 
     return NextResponse.redirect(url);
-};
+}
